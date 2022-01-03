9bb0bc84deda52a356ba3fa7df2c048b38bf2d10
Mitchell Drury <Mitchell.Drury@gmail.com>
2022-01-03 16:22:21 -0600

    add slash to route

--- server/index.js
+++ server/index.js
@@ -12,10 +12,11 @@ app.use(bp.urlencoded({extended: true}));
 
 app.get('/contacts', (req, res) => {
     console.log('fetching all');
-    Contact.findAll({
-        order: ['lastName']
-    })
-    .then(contacts => {res.json(contacts)});
+    res.json([{'firstName': 'mitch'}])
+    // Contact.findAll({
+    //     order: ['lastName']
+    // })
+    // .then(contacts => {res.json(contacts)});
 })
 app.post('/contacts/add', (req, res) => {
     Contact.create(req.body)
