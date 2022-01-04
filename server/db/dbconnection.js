e8280113b30bc6c20cbfe3dd008076096f56c3f7
Mitchell Drury <Mitchell.Drury@gmail.com>
2022-01-03 22:34:37 -0600

    add slash to route

--- server/db/dbconnection.js
+++ server/db/dbconnection.js
@@ -2,7 +2,14 @@ const Sequelize = require('sequelize')
 
 const db = new Sequelize(
   process.env.DATABASE_URL || 'postgres://localhost:5432/reactCalendar', {
-    logging: false
+    "logging": false,
+    "dialect": "postgres",
+    "username": "menqpalmeargts",
+    "password": "d7a9def64c6083e35faaa5affeeb5239a200069d9c8d8f032936decbe529c9de",
+    "dialectOptions": {
+      "ssl": true
+    },
+    "ssl": true
   }
 )
 
