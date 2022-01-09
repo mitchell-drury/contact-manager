f0148767bc9e6ba7f9570aced057982f86e0c473
Mitchell Drury <Mitchell.Drury@gmail.com>
2022-01-09 17:13:41 -0600

    slash *

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bp.urlencoded({extended: true}));
 
 app.use('/api', apiRouter);
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build'), 'index.html');
 })
 
